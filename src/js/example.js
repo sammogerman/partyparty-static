d21c6c2ac3b49e71fdd3ee8b28b2cd1c58c92e1c
moger12345 <55151302+moger12345@users.noreply.github.com>
2020-09-15 11:22:29 -0500

    #12: Corrected many issues with navbar.

--- src/js/example.js
+++
@@ -1,5 +0,0 @@
-// Scripts for individual components will each live in their own file.
-//
-// This is just an example that can be deleted.
-
-console.log('Hello, world');
--- src/js/header.js
+++ src/js/header.js
@@ -2,15 +2,12 @@ let hamburger = document.querySelector(".header__button");
 let navList = document.querySelector(".header__main-nav-list")
 
 //listens for click on nav button and toggles the nav list to be hidden or visible.
-//TODO will also toggle large or small logo
 hamburger.addEventListener("click", function(){
-    if (hamburger.classList.contains(".header__button-toggle")) {
-        hamburger.classList.toggle(".header__button-toggle");
-        navList.classList.add("header__main-nav-list--hidden");
-        hamburger.textContent="Close";
-    } else {
-        hamburger.classList.toggle(".header__button-toggle");
-        hamburger.textContent="Open";
-        navList.classList.remove("header__main-nav-list--hidden");
-    }
+  if (navList.classList.contains("header__main-nav-list--expanded")) {
+    navList.classList.remove("header__main-nav-list--expanded");
+    hamburger.textContent="Open";
+  } else {
+    hamburger.textContent="Close";
+    navList.classList.add("header__main-nav-list--expanded");
+  }
 });
