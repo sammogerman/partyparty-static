8e997e5feee7b86bbfc3fb6d8bbfd8cc0e6c6539
Gus Childs <gus@guschilds.com>
2020-09-29 06:48:10 -0600

    #12: Fixed hiding/showing of mobile menu’s open and close icons.

--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,14 @@
 let headerButton = document.querySelector(".header__button");
-let hamburger = document.querySelector(".header__button-icon--open");
-let closeIcon = document.querySelector(".header__button-icon--close");
 let navList = document.querySelector(".header__main-nav-list");
 
 //listens for click on nav button and toggles the nav list to be hidden or visible.
 headerButton.addEventListener("click", function(){
   if (navList.classList.contains("header__main-nav-list--expanded")) {
-    console.log("clicked");
     navList.classList.remove("header__main-nav-list--expanded");
-    hamburger.style.display = 'block';
-    closeIcon.style.display = 'none';
+    headerButton.classList.remove("header__button--opened");
 
   } else {
-    console.log("a click");
     navList.classList.add("header__main-nav-list--expanded");
-    hamburger.style.display = 'none';
-    closeIcon.style.display = 'block';
+    headerButton.classList.add("header__button--opened");
   }
 });
