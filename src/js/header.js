11298a7179f76f0d16c3aa668ab324655a810cf5
moger12345 <55151302+moger12345@users.noreply.github.com>
2020-10-20 11:53:59 -0500

    #43: Added active class to nav links. Started adding js to add/remove that class.

--- src/js/header.js
+++ src/js/header.js
@@ -12,3 +12,17 @@ headerButton.addEventListener("click", function(){
     headerButton.classList.add("header__button--opened");
   }
 });
+
+//store nav links in array navLinks
+const navLinks = document.querySelectorAll(".header__main-nav-link");
+let currentURL = document.URL;
+
+//loop through URLs of each navLink and see if it contains currentURL
+for (let i = 0; i <= navLinks.length; i++) {
+// check to see if URL matches current page URL
+  if (navLinks[i]["href"] === currentURL) {
+    //if so, add class 'active'
+    navLinks[i].classList.add("header__main-nav-link--active");
+}
+};
+// also need to remove 'active' class if not on current page
