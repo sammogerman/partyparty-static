8f0ca805d9322cf9712600c66707311e1df442c6
moger12345 <55151302+moger12345@users.noreply.github.com>
2020-10-22 09:45:58 -0500

    43: Nested the span style inside the active a tag for the underline. Reworked the JS using suggestions.

--- src/js/header.js
+++ src/js/header.js
@@ -1,39 +1,60 @@
-let headerButton = document.querySelector(".header__button");
-let navList = document.querySelector(".header__main-nav-list");
+//This outer code will ensure the code doesn't execute until all of the elements have been loaded on the page.
+document.addEventListener("DOMContentLoaded", function() {
+  
+  let headerButton = document.querySelector(".header__button");
+  let navList = document.querySelector(".header__main-nav-list");
 
-//listens for click on nav button and toggles the nav list to be hidden or visible.
-headerButton.addEventListener("click", function(){
-  if (navList.classList.contains("header__main-nav-list--expanded")) {
-    navList.classList.remove("header__main-nav-list--expanded");
-    headerButton.classList.remove("header__button--opened");
+  //listens for click on nav button and toggles the nav list to be hidden or visible.
+  headerButton.addEventListener("click", function(){
+    if (navList.classList.contains("header__main-nav-list--expanded")) {
+      navList.classList.remove("header__main-nav-list--expanded");
+      headerButton.classList.remove("header__button--opened");
 
-  } else {
-    navList.classList.add("header__main-nav-list--expanded");
-    headerButton.classList.add("header__button--opened");
-  }
+    } else {
+      navList.classList.add("header__main-nav-list--expanded");
+      headerButton.classList.add("header__button--opened");
+    }
+  });
+
+  const navLinks = document.querySelectorAll(".header__main-nav-link");
+  const currentPage = window.location.pathname;
+
+  // loop through URLs of each navLink and see if it contains currentURL
+  for (let i = 0; i < navLinks.length; i++) {
+    let currentHref = navLinks[i].getAttribute('href');
+  // check to see if URL matches current page URL
+    if (currentPage.includes (currentHref)) {
+      //if so, add class 'active'
+      navLinks[i].classList.add("header__main-nav-link--active");
+    }
+  };
 });
+
+
+
+
 
 
 //1st approach. Seems kinda hacky and might not work once the url is longer 
 //and has info on it after the last backslash that was giving me trouble:
 
-//store nav links in array navLinks
-const navLinks = document.querySelectorAll(".header__main-nav-link");
-//store spans in array navSpans
-const navSpans = document.querySelectorAll(".header__link-text");
-const currentURL = document.URL;
-//newURL cuts off the '/' at the end of the url to make it able to match 
-//the href easier
-const newURL = currentURL.slice(0, currentURL.length - 1);
+// //store nav links in array navLinks
+// const navLinks = document.querySelectorAll(".header__main-nav-link");
+// //store spans in array navSpans
+// const navSpans = document.querySelectorAll(".header__link-text");
+// const currentURL = document.URL;
+// //newURL cuts off the '/' at the end of the url to make it able to match 
+// //the href easier
+// const newURL = currentURL.slice(0, currentURL.length - 1);
 
-//loop through URLs of each navLink and see if it contains currentURL
-for (let i = 0; i < navLinks.length; i++) {
-// check to see if URL matches current page URL
-  if (navLinks[i]["href"] === newURL) {
-    //if so, add class 'active'
-    navSpans[i].classList.add("header__link-text--active");
-  }
-}
+// //loop through URLs of each navLink and see if it contains currentURL
+// for (let i = 0; i < navLinks.length; i++) {
+// // check to see if URL matches current page URL
+//   if (navLinks[i]["href"] === newURL) {
+//     //if so, add class 'active'
+//     navSpans[i].classList.add("header__link-text--active");
+//   }
+// }
 
 
 // ******************
