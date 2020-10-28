ef615c428b383dbc4676cbc49940bdd4fde4b12c
moger12345 <55151302+moger12345@users.noreply.github.com>
2020-10-28 07:22:15 -0500

    [WIP] #53: Began accordion component.

---
+++ src/js/accordion.js
@@ -0,0 +1,12 @@
+let infoOpen = document.querySelector(".accordion__item-icon--expand");
+let infoClose = document.querySelector(".accordion__item-icon--collapse");
+let faqItem = document.querySelector(".accordion__item");
+
+// faqItem.addEventListener("click", function(){
+//   if (faqItem.classList.contains(".accordion__item-icon--expanded") {
+
+//   }
+
+//   ))
+// })
+
