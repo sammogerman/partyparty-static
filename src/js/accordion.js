92dc113d9d1e1cf0a3c9df860e47452607a2b4fb
moger12345 <55151302+moger12345@users.noreply.github.com>
2020-10-28 10:30:48 -0500

    #53: Styled the accordion component. Added js to toggle the accordion.

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,12 +1,13 @@
-let infoOpen = document.querySelector(".accordion__item-icon--expand");
-let infoClose = document.querySelector(".accordion__item-icon--collapse");
-let faqItem = document.querySelector(".accordion__item");
+let faqItems = document.querySelectorAll(".accordion__item");
 
-// faqItem.addEventListener("click", function(){
-//   if (faqItem.classList.contains(".accordion__item-icon--expanded") {
+for (let i = 0; i < faqItems.length; i++) {
+  faqItems[i].addEventListener("click", function(){
+    faqItems[i].classList.toggle("accordion__item--expanded");
+  });  
+}
 
-//   }
-
-//   ))
-// })
+//I know event delegation would be a better way to do this but I need to work on it still:
+// faqItem.addEventListener("click", function(event){
+//   event.target.classList.toggle("accordion__item--expanded");
+// });
 
