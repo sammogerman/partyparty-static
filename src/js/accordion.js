b3b7b8ef9bd600a6ff8d8d75037b1b3468c5d4c4
moger12345 <55151302+moger12345@users.noreply.github.com>
2020-10-30 18:47:48 -0500

    #53: Deleted comment and unnecessary code from JS.

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,15 +1,7 @@
 const faqs = document.querySelectorAll(".accordion__item-title");
-const accordionItems = document.querySelectorAll(".accordion__item");
 
 for (let i = 0; i < faqs.length; i++) {
   faqs[i].addEventListener("click", function(){
     faqs[i].parentNode.classList.toggle("accordion__item--expanded");
   });  
 }
-
-// WIP Version 2 for practice/funsies using Event delegation.
-// const accordion = document.querySelector(".accordion");
-// accordion.addEventListener("click", function(event){
-//   event.currentTarget.classList.toggle("accordion__item--expanded");
-// });
-
